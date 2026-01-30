3cb594d05c4a3ee41bc4c5699f4dd64d1a47aa1a
RychenJones <rychenjones66@gmail.com>
2026-01-30 15:48:52 -0700

    today

---
+++ javascript/universal.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('#ham-menu');
+let menu = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+        menu.classList.toggle('hide');
+        btn.classList.toggle('change');
+}
