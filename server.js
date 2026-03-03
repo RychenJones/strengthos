fbd5ab9f0144bb456e24d34f1d96430d3780479c
RychenJones <rychenjones66@gmail.com>
2026-03-03 10:38:24 -0700

    r

--- server.js
+++ server.js
@@ -15,34 +15,86 @@ app.post('/submit-form', (req, res) => {
   res.send('Form received');
 });
 
-// route for add workout
-app.post("/add-workout", (req,res)=>{
-  const { name, date, duration, intensity } = req.body;
+// routes
 
-  console.log("Workout:", name);
-  console.log("Date:", date);
-  console.log("Duration:", duration);
-  console.log("Intensity:", intensity);
+  // route for login
+  app.post("/login", (req,res)=>{
+    const { email, password } = req.body;
 
-  // Handle multiple exercises
-  const exercises = [];
-  if (req.body.exercise) {
-    for (let i = 0; i < req.body.exercise.length; i++) {
-      exercises.push({
-        exercise: req.body.exercise[i],
-        weight: req.body.weight[i],
-        unit: req.body.unit[i],
-        sets: req.body.sets[i],
-        reps: req.body.reps[i]
-      });
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // respond
+    res.send("Logged in");
+  });
+
+  // route for account creation
+  app.post("/create-account", (req,res)=>{
+    const { name, birthdate, email, password, confirm } = req.body;
+
+    console.log("Name:", name);
+    console.log("Birthdate:", birthdate);
+    console.log("Email", email);
+    console.log("Password", password);
+    console.log("Confirm Password", confirm);
+
+    // respond
+    res.send("Received");
+  });
+
+  // route for account creation
+  app.post("/questionaire", (req,res)=>{
+    const { weight, unit, fitnessGoal, experience, daysPerWeek } = req.body;
+
+    console.log("Weight:", weight);
+    console.log("Unit:", unit);
+    console.log("Fitness Goal", fitnessGoal);
+    console.log("Experience", experience);
+    console.log("Days per week", daysPerWeek)
+
+    // respond
+    res.send("Account created");
+  });
+
+  // route for password reset
+  app.post("/reset", (req,res)=>{
+    const { pw, confirmPw } = req.body;
+
+    console.log("Password:", pw);
+    console.log("Confirm password:", confirmPw);
+
+    // respond
+    res.send("Password reset");
+  });
+
+  // route for add workout
+  app.post("/add-workout", (req,res)=>{
+    const { name, date, duration, intensity } = req.body;
+
+    console.log("Workout:", name);
+    console.log("Date:", date);
+    console.log("Duration:", duration);
+    console.log("Intensity:", intensity);
+
+    // Handle multiple exercises
+    const exercises = [];
+    if (req.body.exercise) {
+      for (let i = 0; i < req.body.exercise.length; i++) {
+        exercises.push({
+          exercise: req.body.exercise[i],
+          weight: req.body.weight[i],
+          unit: req.body.unit[i],
+          sets: req.body.sets[i],
+          reps: req.body.reps[i]
+        });
+      }
     }
-  }
 
-  console.log("Exercises:", exercises);
+    console.log("Exercises:", exercises);
 
-  // respond
-  res.send("Workout logged");
-});
+    // respond
+    res.send("Workout logged");
+  });
 
 // Start the server
 const PORT = 3000;
